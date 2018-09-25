3d2d1bfb209f5817973459adc85aa4f8765fd47b
gscigliotto <gscigliotto@gmail.com>
2018-09-25 19:00:18 -0300

    Mejoras menu

--- Delibiery/WebApp/Models/AutenticacionModel.cs
+++ Delibiery/WebApp/Models/AutenticacionModel.cs
@@ -85,8 +85,8 @@ namespace WebApp.Models
     {
         public string descripcion { get; set; }
 
-        public string action { get; set; }
-        public string controller { get; set; }
+        public string actionName { get; set; }
+        public string controllerName { get; set; }
         public string roles { get; set; }
     }
 }
